c81ec31fef6d2f26316f55cc3e982586b9c624bd
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 00:39:55 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "0d85d8fc-18cb-4d4b-a399-0e272eb237c9",
+    "home_feed": [
+        {
+            "id": "a4cc26ca-ffff-449b-bac6-0e04ed81919b",
+            "artist_name": "list",
+            "home_carousel": [
+                {
+                    "id": "c8edf340-9624-4e69-b3d6-e9ec4cf88214",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/156.png"
+                },
+                {
+                    "id": "743022fb-fc80-40ff-8b16-9a046437d9da",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png"
+                },
+                {
+                    "id": "f307c19d-6a18-437d-ade8-93e8f6e3719f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png"
+                },
+                {
+                    "id": "14c5f8f2-9e81-40b4-809c-292c82ea3438",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/136.png"
+                },
+                {
+                    "id": "c7d72a75-c41c-47b5-a25e-13bfbb0f4290",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                }
+            ],
+            "art_caption": "shake",
+            "art_description": "Movie doctor fight this wind. Resource stage morning social. Finish more free whether."
+        },
+        {
+            "id": "ca96982d-d987-4f49-acd5-47fa4d12b809",
+            "artist_name": "enter",
+            "home_carousel": [
+                {
+                    "id": "d1c3864a-c601-4178-a82a-110664cafb2d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                },
+                {
+                    "id": "f308f556-cccc-469e-b29a-b00aead8fd2d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/61.png"
+                },
+                {
+                    "id": "52a1b3ed-5198-4cd1-9f17-343d429bc204",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/310.png"
+                },
+                {
+                    "id": "d65c8ff3-c2e3-48c7-89e8-bfd7b9ee0b36",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png"
+                },
+                {
+                    "id": "5b60f84b-81c8-4958-9a4f-ce56844079a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                }
+            ],
+            "art_caption": "reason",
+            "art_description": "Student together body job reach. Congress treat summer."
+        },
+        {
+            "id": "52dcc4d1-6941-4de4-a9e3-92a33aa14c0b",
+            "artist_name": "travel",
+            "home_carousel": [
+                {
+                    "id": "c6285c0d-8025-4de0-bfbf-c4bfb6fe511b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                },
+                {
+                    "id": "26639311-718e-4846-8083-2bdfd95ebf98",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/293.png"
+                },
+                {
+                    "id": "b69b2d81-7213-45c2-9ad7-991b867a76b2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                },
+                {
+                    "id": "11bdd9e4-ec42-4c90-9a19-315e29204417",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png"
+                },
+                {
+                    "id": "6a3d57fc-d3ad-47d5-8efc-f350a8b3735c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                }
+            ],
+            "art_caption": "change",
+            "art_description": "Unit building course teacher civil above democratic. Friend help attention out difference ability western thank. Difficult will expect wall determine natural rich."
+        },
+        {
+            "id": "e1bee43e-9bc4-45c7-b54d-238344343820",
+            "artist_name": "two",
+            "home_carousel": [
+                {
+                    "id": "a001bc10-63d4-4048-9144-1076ef46f6a1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "579593b5-e33d-4153-8895-f5fed77c1aa8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png"
+                },
+                {
+                    "id": "a4e00842-faad-47a9-b279-76b9650cf705",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "c8a24809-daae-4bd1-aee1-13e20ef5009e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/68.png"
+                },
+                {
+                    "id": "3a130171-025a-4d29-aa5d-174fe4195775",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                }
+            ],
+            "art_caption": "of",
+            "art_description": "Finish money particular thing. Carry agent or race only learn feeling. Shake conference interesting radio will."
+        },
+        {
+            "id": "ba1aad56-1267-40b8-8910-c7d6f0abf0f3",
+            "artist_name": "simply",
+            "home_carousel": [
+                {
+                    "id": "d27ce02a-ea65-4b29-ac73-e8459c57ff77",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                },
+                {
+                    "id": "b063ce16-f2ae-4024-9d1c-b8fb61700b80",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png"
+                },
+                {
+                    "id": "88350b70-62fd-4709-a4ab-6c47de4e5a45",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "f54eadcd-6bc9-4bdb-9e0a-33383ba0d9f7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                },
+                {
+                    "id": "0fb5cf6d-75d4-437e-8ac0-77aa1c350c3d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png"
+                }
+            ],
+            "art_caption": "nation",
+            "art_description": "Blue statement start woman cell. Dream drive imagine task better skill business. Social candidate attorney decision hard. Firm than her deep left bar."
+        }
+    ]
+}
